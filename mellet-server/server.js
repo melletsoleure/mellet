312c817f73508f4c703a47a1b6d07878ff15c39d
Alim Weber <melletsoleure@gmail.com>
2022-08-03 13:21:04 +0200

    server initialization

---
+++ mellet-server/server.js
@@ -0,0 +1,21 @@
+const { Router } = require("express");
+const {graphqlHTTP}  = require("express-graphql")
+const {buildSchema}  = require("graphql")
+const express = require("express")
+const app = require("express")()
+const path = require("path")
+
+
+app.use(express.static('dist'));
+
+
+
+
+app.use("/api", Router().get("/api", (req, res) => {
+   res.send("some files")
+}))
+
+app.get('*', (req, res) => {
+   res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
+app.listen(5000 || process.env.PORT, () => console.log("APP runs"))
