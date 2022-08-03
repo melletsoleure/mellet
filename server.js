b6c80663dfc7573e964a94274202d55c6259f3ae
Alim Weber <melletsoleure@gmail.com>
2022-08-03 15:06:19 +0200

    changed server file

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const { Router } = require("express");
+const {graphqlHTTP}  = require("express-graphql")
+const {buildSchema}  = require("graphql")
 const express = require("express")
 const app = require("express")()
 const path = require("path")
 
 
 app.use(express.static('dist'));
-
 
 
 
@@ -16,8 +17,4 @@ app.use("/api", Router().get("/api", (req, res) => {
 app.get('*', (req, res) => {
    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
-});
+app.listen(process.env.PORT || 5000, () => console.log("APP runs"))
